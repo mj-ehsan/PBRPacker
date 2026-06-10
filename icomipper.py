a1e292a4da8aec47caa80d5983fdf115e8c7b60c
mj-ehsan <86712594+mj-ehsan@users.noreply.github.com>
2026-06-10 15:24:58 +0330

    Delete icomipper.py
    removed irrelevant script that is not part of the build. I used it only to generate mips for the icon.

--- icomipper.py
+++
@@ -1,5 +0,0 @@
-from PIL import Image
-
-img = Image.open("C:/PBRPacker/PBRPacker.ico")
-icon_sizes = [(16, 16), (32, 32), (48, 48), (64, 64), (128, 128), (256, 256)]
-img.save("C:/PBRPacker/app_icon.ico", format="ICO", sizes=icon_sizes)
